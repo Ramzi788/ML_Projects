d89006e8f7f1e65c3a90ef2572ff039287af9259
Ramzi788 <Rzeineddine200382@gmail.com>
2026-03-11 23:31:37 -0400

    app submission

--- DeepLearning/semantic-segmentation/semantic_segmentation.py
+++ DeepLearning/semantic-segmentation/semantic_segmentation.py
@@ -92,11 +92,11 @@ def semantic_segmentation(model_type="base"):
 
         # specify train_opts — SGD with Nesterov and multi-step decay
         train_opts = {
-            'lr': 0.001,
+            'lr': 0.05,
             'num_epochs': 500,
             'momentum': 0.9,
             'gamma': 0.1,
-            'weight_decay': 1e-4,
+            'weight_decay': 1e-3,
             'batch_size': 24,
             'milestones': [100, 180, 250],
             'objective': CrossEntropyLoss(weight=class_weights, label_smoothing=0.1),
